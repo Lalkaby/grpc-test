e3be8cc10ed2b21542d633392de1daad730c99ba
Yan Temniakov <yan.tsemniakou@softclub.by>
2024-01-22 16:07:38 +0300

    rpc test

--- src/main/java/by/temniakov/grpc/services/ExceptionHandler.java
+++ src/main/java/by/temniakov/grpc/services/ExceptionHandler.java
@@ -4,14 +4,12 @@ import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @GrpcAdvice
 public class ExceptionHandler {
 
     @GrpcExceptionHandler(IllegalArgumentException.class)
-    public StatusRuntimeException handleResourceNotFoundException(IllegalArgumentException error){
+    public StatusRuntimeException handleException(IllegalArgumentException error){
 
         return Status.CANCELLED
                 .withDescription("abracadaabra "+ error.getMessage())
--- src/main/java/by/temniakov/grpc/services/HelloServiceImpl.java
+++ src/main/java/by/temniakov/grpc/services/HelloServiceImpl.java
@@ -3,12 +3,8 @@ package by.temniakov.grpc.services;
 import by.temnaikov.grpc.model.HelloReply;
 import by.temnaikov.grpc.model.HelloRequest;
 import by.temnaikov.grpc.services.HelloServiceGrpc;
-import io.grpc.Metadata;
-import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.hibernate.boot.model.naming.IllegalIdentifierException;
 
 @GrpcService
 public class HelloServiceImpl  extends HelloServiceGrpc.HelloServiceImplBase{
